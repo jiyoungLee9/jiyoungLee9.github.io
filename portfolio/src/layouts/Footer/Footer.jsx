dd0a7f3d211537f4b5bc659efa6a4f890b2a28f3
leejiyoung <jet052@gmail.com>
2026-04-09 14:35:54 +0900

    fix : seo이미지 추가 및 footer 이메일 추가

--- portfolio/src/layouts/Footer/Footer.jsx
+++ portfolio/src/layouts/Footer/Footer.jsx
@@ -1,9 +1,20 @@
 import styles from './Footer.module.scss'
 
 const Footer = () => {
+
+  const email = "jadelee.project@gmail.com";
+
   return (
     <footer className={styles.footer}>
       <p className={styles.copy}>© 2026 Jiyoung Lee. All rights reserved.</p>
+      <a 
+          href={`mailto:${email}`} 
+          className={styles.footer_link}
+          aria-label="이메일 보내기 (새 창 열림)"
+          title="이메일 보내기 (새 창 열림)"
+        >
+          {email}
+        </a>      
     </footer>
   )
 }
